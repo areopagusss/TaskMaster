e5701116689c51abc5c5da9d7f8471ab6c073a81
Goro-Madjima111 <a.karagadian2001@gmail.com>
2024-12-25 00:43:09 +0300

    Добавил структуру данных + добавил страницу анкеты (не полный)

---
+++ WpfApp1/Views/Pages/SignIn.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class SignIn : Page
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+        private void Button_RegMain_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new SignUp();
+        }
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new MainPage();
+        }
+    }
+}
---
+++ WpfApp1/Views/Pages/SignUp.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class SignUp : Page
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = TextBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                TextBoxLogin.ToolTip = "Логин должен быть больше 5 символов";
+                TextBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Пароль должен быть больше 5 символов";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                TextBoxEmail.ToolTip = "Укажите корректную почту";
+                TextBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxLogin.ToolTip = "";
+                TextBoxLogin.Background = Brushes.Transparent;
+
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+
+                TextBoxEmail.ToolTip = "";
+                TextBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Пользователь зарегистирован");
+
+                MyFrame.Content = new QuestionnairePage();
+            }
+        }
+        private void Button_Window1_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new SignIn();
+        }
+    }
+}
